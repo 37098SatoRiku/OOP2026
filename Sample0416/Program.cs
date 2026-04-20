104832e5488b61ad9e7cdafe46fa7fe1167d756e
佐藤 莉玖 <oibc37098@ojs.ac.jp>
2026-04-20 10:23:59 +0900

    繰り返し回数の表示

--- Sample0416/Program.cs
+++ Sample0416/Program.cs
@@ -2,12 +2,15 @@
 {
     internal class Program
     {
-        static void Main(string[] args){
+        static void Main(string[] args)
+        {
             int i = 0;
-            while (i < 10) {
+            while (i < 10)
+            {
                 Console.WriteLine("Hello, World!");
                 i++;
             }
+            Console.WriteLine(i + "回繰り返しました。");
         }
     }
 }
