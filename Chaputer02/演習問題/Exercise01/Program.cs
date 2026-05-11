1f47c4723a961445ea6da6e1437b6be4849d6efd
佐藤 莉玖 <oibc37098@ojs.ac.jp>
2026-05-11 10:44:00 +0900

    演習問題2.1.4まで

---
+++ Chaputer02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var songs = new Song[]{
+            new Song("Let it be","The Beatles",243),
+            new Song("Bridge Over Troubled Water","Simon & Garfunkel",293),
+            new Song("Close To You","Carpenters",276),
+            new Song("Honesty","Billy Joel",231),
+            new Song("I Will Always Love You","Whitney Houston",273)
+            };
+            PrintSongs(songs);
+        }
+        //Mainメソッド内のPrintSongs(songs);をクリックしてAlt+Enterを押すと以下のメソッドが自動的に作成される
+        private static void PrintSongs(Song[] songs) {
+            foreach(var Song in songs) {
+                Console.WriteLine(Song.Title + "," + Song.ArtistName + "," + Song.Length / 60 + ":" + (Song.Length % 60).ToString("00"));
+            }
+        }
+    }
+}
