70179842a75192f6d142cebbd4c73487d99442a4
佐藤 莉玖 <oibc37098@ojs.ac.jp>
2026-05-13 11:51:21 +0900

    code3-1

---
+++ Chaputer02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    public static class InchConverter {
+        public static readonly double ratio = 0.0254;
+        // メートルからインチを求める
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //インチからメートルを求める
+        public static double ToMeter(double Inch) {
+            return Inch * ratio;
+        }
+    }
+}
