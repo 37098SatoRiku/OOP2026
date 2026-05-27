a65fc89e7b4a6717ead826bb90426b0cdac1e968
佐藤 莉玖 <oibc37098@ojs.ac.jp>
2026-05-27 11:10:51 +0900

    演習問題4-2

---
+++ Chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,71 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(" --- 4.2.1 ---");
+            Exercise1();
+            Console.WriteLine("\n --- 4.2.2 ---");
+            Exercise2();
+            Console.WriteLine("\n --- 4.2.3 ---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            //if-else文を使用
+            Console.Write("整数");
+            var line = Console.ReadLine();
+            if(int.TryParse(line, out var number)) {
+            } else {
+                Console.WriteLine("入力値に誤りがあります。");
+            }
+            if(number < 0) {
+                Console.WriteLine(number);
+            } else if(number < 100) {
+                Console.WriteLine(number * 2);
+            } else if(number < 500) {
+                Console.WriteLine(number * 3);
+            } else {
+                Console.WriteLine(number);
+            }
+        }
+
+        private static void Exercise2() {
+            //switch文を使用
+            Console.Write("整数");
+            if(int.TryParse(Console.ReadLine(), out var number)) {
+                switch(number) {
+                    case < 0:
+                        Console.WriteLine(number);
+                        break;
+                    case < 100:
+                        Console.WriteLine(number * 2);
+                        break;
+                    case < 500:
+                        Console.WriteLine(number * 3);
+                        break;
+                    default:
+                        Console.WriteLine(number);
+                        break;
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります。");
+            }
+        }
+
+        private static void Exercise3() {
+            //switch式を使用
+            Console.Write("整数");
+            if(int.TryParse(Console.ReadLine(), out var number)) {
+                var result = number switch {
+                    < 0 => number,
+                    < 100 => number * 2,
+                    < 500 => number * 3,
+                    _ => number,
+                };
+                Console.WriteLine(result);
+            } else {
+                Console.WriteLine("入力値に誤りがあります。");
+            }
+        }
+    }
+}
