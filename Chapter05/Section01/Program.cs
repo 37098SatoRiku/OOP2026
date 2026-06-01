d1d08180f12ad04d847aff742bf7f167d6b04876
佐藤 莉玖 <oibc37098@ojs.ac.jp>
2026-06-01 10:31:39 +0900

    演習問題5.1

---
+++ Chapter05/Section01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Immutable;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //var obj = new PasswordPolicy("aaaaa", "bbbbb");
+            //var data = obj.Name;
+            var ms = new MySaple();
+            var newList = ms.MyList.Add(6).RemoveAt(0);
+            ms.MyList.ForEach(n => Console.Write($"{n}"));
+            Console.WriteLine();
+
+            newList.ForEach(n => Console.Write($"{n}"));
+            Console.WriteLine();
+        }
+    }
+
+    class MySaple {
+        public ImmutableList<int> MyList { get; set; }
+
+        public MySaple() {
+            var list = new List<int> ()  { 1,2,3,4,5};
+            MyList = list.ToImmutableList();
+        }
+    }
+
+    class PasswordPolicy {
+        //プロパティの初期化
+        public int MinimumLength { get; set; } = 8;
+
+        //読み取り専用プロパティ
+        public string GivenName { get; private set; } = null!;
+        public string FamilyName { get; init; } = null!;
+
+        /*getアクセサーのみを定義した読み取り専用プロパティ
+        public string Name {
+            get { return FamilyName + " " + GivenName; }
+        }*/
+
+        public string Name => FamilyName + " " + GivenName;
+
+        public PasswordPolicy(string familiName, string GivenName) {
+            FamilyName = FamilyName;
+            GivenName = GivenName;
+        }
+    }
+}
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1         
+    public class YearMonth(int Year, int Month, int Is21Century) {
+        public int Year { get; private set; } = Year;
+        public int Month { get; private set; } = Month;
+        public bool Is21Century => Year <= 2001 && 2100 >= Year;
+
+        public YearMonth AddOneMonth() {
+            if(Month == 12) {
+                return new YearMonth(Year + 1, 1, Year);
+            }
+            return new YearMonth(Year, Month + 1, Year);
+        }
+
+        public override string ToString() => $"{Year}年{Month}月";
+    }
+}
+
