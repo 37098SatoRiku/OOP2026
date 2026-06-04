f3d3554aed8e16eed7481742e61eac6942c2fe36
佐藤 莉玖 <oibc37098@ojs.ac.jp>
2026-06-04 10:04:39 +0900

    演習問題5.2

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -6,16 +6,22 @@ using System.Threading.Tasks;
 
 namespace Exercise01 {
     //5.1         
-    public class YearMonth(int Year, int Month, int Is21Century) {
+    public record YearMonth(int Year, int Month) {
         public int Year { get; private set; } = Year;
         public int Month { get; private set; } = Month;
-        public bool Is21Century => Year <= 2001 && 2100 >= Year;
 
-        public YearMonth AddOneMonth() {
-            if(Month == 12) {
-                return new YearMonth(Year + 1, 1, Year);
+        public bool Is21Century() { 
+          if(Year >= 2001 && 2100 >= Year) {
+                return true;
             }
-            return new YearMonth(Year, Month + 1, Year);
+            return false;
+        }
+
+        public  YearMonth AddOneMonth() {
+            if(Month == 12) {
+                return new YearMonth(Year + 1, 1);
+            }
+            return new YearMonth(Year, Month + 1);
         }
 
         public override string ToString() => $"{Year}年{Month}月";
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,51 @@
+﻿using Exercise01;
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            // 5.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2024, 12),
+            };
+
+            Console.WriteLine("5.2.2");
+            Exercise2(ymCollection);
+
+            Console.WriteLine("5.2.4");
+            var result = Exercise4(ymCollection);
+            if(result is not null) {
+                Console.WriteLine(result);
+            } else {
+                Console.WriteLine("21世紀のデータはありません。");
+            }
+
+            Console.WriteLine("5.2.5");
+            Exercise5(ymCollection);
+        }
+
+        private static void Exercise2(YearMonth[] ymCollection) {
+            foreach(var item in ymCollection) {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static YearMonth Exercise4(YearMonth[] ymCollection) {
+            foreach(var item in ymCollection) {
+                if(item.Is21Century()) {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        private static void Exercise5(YearMonth[] ymCollection) {
+            YearMonth[] result = ymCollection.Select(x => x.AddOneMonth()).ToArray();
+            foreach(var item in result) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
