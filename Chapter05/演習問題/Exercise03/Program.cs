831364497f5a267dc8bcfc306900a86c80cd7dec
佐藤 莉玖 <oibc37098@ojs.ac.jp>
2026-06-08 11:18:16 +0900

    record型

---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exercise01;
+using System.Security.Cryptography;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var ym1 = new YearMonth(2002, 10);
+            var ym2 = new YearMonth(2002, 10);
+            if(ym1 == ym2) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
